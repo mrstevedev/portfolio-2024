79ace40b402f6277707a4e0b3a85a413f2b4cd9a
Steven <stevendotpulido@gmail.com>
2024-11-22 10:22:20 -0800

    feat: Add Projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,32 @@
+import BBP from "@/components/Projects/BBP/BBP";
+import Rivian from "@/components/Projects/Rivian/Rivian";
+import { Box, Container, Flex } from "@radix-ui/themes";
+
+import { Love_Light } from "next/font/google";
+
+const lovelight = Love_Light({ weight: "400", subsets: ["latin"] });
+
+export default function Projects() {
+  return (
+    <Container className="p-4 lg:p-32 xl:w-[700px] mx-auto">
+      <h1 className="text-center text-2xl">My Recent Projects</h1>
+      <p className="text-center text-lg">
+        Below are a few of my current{" "}
+        <span
+          className={lovelight.className}
+          style={{ fontSize: "24px", color: "#A1B4E4" }}
+        >
+          &
+        </span>{" "}
+        past projects
+      </p>
+
+      <Box className="m-4">
+        <Flex className="gap-4 justify-center items-center">
+          <BBP />
+          <Rivian />
+        </Flex>
+      </Box>
+    </Container>
+  );
+}
