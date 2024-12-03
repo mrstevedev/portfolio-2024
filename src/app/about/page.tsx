839b0002e2f6f3017638b9caccb94d4f31a70db5
Steven <stevendotpulido@gmail.com>
2024-12-02 19:51:39 -0800

    feat: add socials

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,15 @@
+/* eslint-disable @next/next/no-img-element */
 import { Box, Container, Flex } from "@radix-ui/themes";
 import { Love_Light } from "next/font/google";
-import Image from "next/image";
-import vinylImg from "@/public/images/vinyl_image.webp";
-import collectionOne from "@/public/images/collection1.jpg";
 import collectionTwo from "@/public/images/collection2.jpg";
+import Link from "next/link";
 
 const lovelight = Love_Light({ weight: "400", subsets: ["latin"] });
 
 export default function page() {
   return (
     <Container className="p-4 lg:p-32 xl:w-[1200px] mx-auto">
-      <Flex className="flex">
+      <Flex>
         <Box className="w-full">
           {" "}
           <h1 className="w-[550px] mx-auto text-2xl">About Me</h1>
@@ -35,17 +34,38 @@ export default function page() {
         </Box>
       </Flex>
       <Flex className="pt-4">
-        <Box className="w-full">
-          <Image
-            src={collectionTwo}
+        <Box className="m-auto flex flex-col">
+          <img
+            src={collectionTwo.src}
             alt="My Vinyl Record Collection"
             width={550}
             height={550}
-            className="w-[550px] mx-auto rounded-lg"
+            className="w-[550px] mx-auto rounded-lg transition-all hover:opacity-90"
           />
-          <p className="w-[550px] mx-auto mt-[0.5rem] text-sm italic">
-            The photo above is my vinyl record collection
-          </p>
+          <Flex className="m-auto w-full items-center">
+            <p className="mx-auto m-[0.5rem] text-sm italic w-full">
+              The photo above is my vinyl record collection
+            </p>
+          </Flex>
+
+          <Box className="m-auto w-full">
+            <Link
+              href="https://www.discogs.com/user/eckosneekz"
+              target="_blank"
+              className="text-xs border-b hover:border-0 active:bg-slate-700 p-0.3"
+            >
+              I&apos;m on Discogs!
+            </Link>
+          </Box>
+          <Box className="m-auto w-full">
+            <Link
+              href="https://www.instagram.com/spdaillkid"
+              target="_blank"
+              className="text-xs border-b hover:border-0 active:bg-slate-700 p-0.3"
+            >
+              I&apos;m on Instagram!
+            </Link>
+          </Box>
         </Box>
       </Flex>
     </Container>
