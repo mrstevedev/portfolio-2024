1d4835517be8629f170642ca048800282d2fe9d7
Steven <stevendotpulido@gmail.com>
2024-12-06 13:02:08 -0800

    feat: add dynamic date for the current year

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,12 +3,13 @@ import { Allura } from "next/font/google";
 const allura = Allura({ weight: "400", subsets: ["latin"] });
 
 export default function Footer() {
+  const date = new Date().getFullYear();
   return (
     <Box
       className="bg-black w-full h-26 p-3 fixed bottom-0 flex items-center gap-2"
       style={{ justifyContent: "flex-end" }}
     >
-      <Text className="text-white text-xs">© 2023</Text>
+      <Text className="text-white text-xs">© {date}</Text>
       <Text className={allura.className}>Steven Pulido</Text>
     </Box>
   );
